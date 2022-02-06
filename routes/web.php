3d16f3b9bfac3df5ed3f1db7c19edf12cfbc0f7b
Zain <zaiabdullah99@gmail.com>
2022-02-06 14:04:27 +0700

    (feat) Install auth

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,17 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/detail', 'DetailController@index')->name('detail');
 Route::get('/checkout', 'CheckoutController@index')->name('checkout');
 Route::get('/checkout/success', 'CheckoutController@success')->name('checkout-success');
 
-Route::prefix('admin')->namespace('Admin')->group(function () {
+// middleware auth dan amin utk keamanan dari Kernel 
+Route::prefix('admin')->namespace('Admin')->middleware('auth', 'admin')->group(function () {
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
+
+Auth::routes();
